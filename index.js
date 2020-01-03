81074f78d089de4c5beaf1bd0811d59e4843df90
Rob Lawrence <iamrobnyce@gmail.com>
2020-01-03 19:56:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+var musicianString;
+var musicianInstrument=[];
+for(var i=0; i<musicians.length; i++){
+musicianString= musicians[i] + ‘ plays ‘ + instruments[i];
+musicianInstrument[i]=musicianString;
+}
+return musicianInstrument;
+}
