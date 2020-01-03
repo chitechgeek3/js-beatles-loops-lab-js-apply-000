fb9c332f8d5eacc6b6dc4cab9ca5a6a3ea1e98b2
Rob Lawrence <iamrobnyce@gmail.com>
2020-01-03 20:04:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
+var johnLennonFacts = function (factsArray) {
+	var i = 0;
+	var arrayCounter = factsArray.length
+	var newFactsArray = [];
+
+	while (arrayCounter > 0) {
+		newFactsArray.push(factsArray[i] + "!!!");
+		i++;
+		arrayCounter--;
+	}
+	return(newFactsArray);
+};
+
+johnLennonFacts(facts);
 
 function iLoveTheBeatles(number) {
   var array = [];
